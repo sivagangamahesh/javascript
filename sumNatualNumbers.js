c1be681e91c9f7a7024102da31b60a5800a0ca59
Ganga Mahesh <sivagangamahesh@gmail.com>
2020-11-23 09:47:31 +0530

    console is kept

---
+++ sumNatualNumbers.js
@@ -0,0 +1,9 @@
+// sum of n natural numbers
+let sumNatualNumbers = function(num)
+{
+    for(let i=1; i<=num; i++)
+    sumvalue += num
+    return sumNatualNumbers
+
+}
+console.log(sumNatualNumbers(10))
